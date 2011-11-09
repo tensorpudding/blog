3f57f3ac3896272456e5c16db32fe6790f2dd970
Michael Moorman <tensorpuddin@devio.us>
2011-11-08 20:33:03 -0500

    sort posts in descending order

--- src/blog/models/posts.clj
+++ src/blog/models/posts.clj
@@ -19,6 +19,7 @@
                                      :time (sql-current-time)
                                      :title title
                                      :body body})))
+
 (defn get-post "Get post by pid"
   [pid] (let [result (select posts
                               (where {:pid pid})
@@ -31,5 +32,13 @@
 (defn get-posts-by-user "Get all posts owned by uid"
   [uid]
   (let [result (select posts
-                       (where {:uid uid}))]
+                       (where {:uid uid})
+                       (order :time :DESC))]
     (map (fn [x] (assoc x :time (parse-sql-time (:time x)))) result)))
+
+(defn get-last-n-posts "Get the last n posts"
+  [n]
+  (let [results (select posts
+                        (limit n)
+                        (order :time :DESC))]
+    (map (fn [x] (assoc x :time (parse-sql-time (:time x)))) results)))
